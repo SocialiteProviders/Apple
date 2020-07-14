5d16e322e988115241c51dfd8e60a1ed440efb92
Albert Vásquez <masticore252@gmail.com>
2020-07-14 00:48:33 -0300

    More fixes

--- Provider.php
+++ Provider.php
@@ -196,7 +196,7 @@ class Provider extends AbstractProvider
     protected function mapUserToObject(array $user)
     {
         $requestValue = request('user');
-        if (! is_bool($requestValue) && ! is_array($requestValue) && trim((string) $requestValue) !== '') {
+        if (is_bool($requestValue) || is_array($requestValue) || trim((string) $requestValue) !== '') {
             $userRequest = json_decode(request('user'), true);
 
             if (array_key_exists('name', $userRequest)) {
